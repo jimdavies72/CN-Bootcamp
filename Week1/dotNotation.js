3fad8d680d3c86a63fb00917135632c347ec554b
jimdavies72 <60436624+jimdavies72@users.noreply.github.com>
2021-11-15 17:17:27 +0000

    Initial

---
+++ Week1/dotNotation.js
@@ -0,0 +1,29 @@
+const gridArray = ["___|___|___", "___|___|___", "   |   |"];
+
+for (i=0; i < gridArray.length; i++){
+  console.log (gridArray[i])
+}
+
+const myArray = [
+  "Apples",
+  "Pears",
+  "Banana"
+]
+
+for (i=0; i < myArray.length; i++){
+  console.log(`Fruit: ${myArray[i]}`)
+}
+
+console.log(myArray)
+myArray.unshift("Guarva")
+console.log(myArray);
+myArray.push("Orange")
+console.log(myArray);
+myArray.shift()
+console.log(myArray);
+myArray.pop()
+console.log(myArray);
+myArray.push("Grape")
+console.log(myArray);
+myArray.splice(1,1)
+console.log(myArray);
