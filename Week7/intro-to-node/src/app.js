a24014557955af92f2a9df6f6894ba04fbbf1bac
James Davies <60436624+jimdavies72@users.noreply.github.com>
2022-01-05 08:50:03 +0000

    week7 created intro-to-node folder and moved code there. added no file exists handle for quit/review options.

--- Week7/intro-to-node/src/app.js
+++ Week7/intro-to-node/src/app.js
@@ -5,18 +5,20 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const { userChoices, movieQuestions } = require("./utils/questionArrays.js");
 const { addMovie } = require("./utils/index");
+const filePath = "./storage.json";
 
 const app = async (clearConsole = true) => {
+  let movieArray;
+
   if (clearConsole) {
     console.clear();
   }
 
   console.log("\n");
-  let movieArray;
   let response = await inquirer.prompt(userChoices);
   if (response.addQuitOrReview === "Add") {
     try {
-      movieArray = JSON.parse(fs.readFileSync("./storage.json"));
+      movieArray = JSON.parse(fs.readFileSync(filePath));
       //console.log(movieArray);
     } catch (error) {
       movieArray = [];
@@ -31,18 +33,26 @@ const app = async (clearConsole = true) => {
     displayMovies();
     app(false);
   } else {
+    //quit
     displayMovies();
   }
 };
 
 const displayMovies = () => {
-  console.clear();
-  console.log(`
-  Stored Movies...
-  ________________
-    `);
-  console.log(getMovies());
-  console.log("\n");
+  if (fs.existsSync(filePath)) {
+    console.clear();
+    console.log(`
+    Stored Movies...
+    ________________
+      `);
+    console.log(getMovies());
+    console.log("\n");
+  } else {
+    console.clear();
+    console.log("\n");
+    console.log("No movies currently stored!");
+    console.log("\n");
+  }
 };
 
 app();
